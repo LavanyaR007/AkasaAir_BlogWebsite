72d7346e7673dbe4b15b5677e10136f4e3cb1363
LavanyaR007 <75923059+LavanyaR007@users.noreply.github.com>
2023-12-05 19:49:20 +0530

    Delete user directory

--- user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('myprofile', views.userprofile, name='myprofile'),
-    path('userupdate',views.user_update,name='userupdate')
-]
